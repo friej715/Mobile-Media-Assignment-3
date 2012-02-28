7fecec105f94dcd32e9d58a4449ff4ff39b40c3d
Jane <friej715@newschool.edu>
2012-02-27 20:55:15 -0500

    Got first review showin'

---
+++ Restaurants/Review.m
@@ -0,0 +1,15 @@
+//
+//  Review.m
+//  Restaurants
+//
+//  Created by Jane Friedhoff on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Review.h"
+
+@implementation Review
+
+@synthesize reviewer, text, score, numberOfHelpfulReviews, numberOfUnhelpfulReviews;
+
+@end

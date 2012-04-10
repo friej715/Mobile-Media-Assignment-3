e8b3c56f455a2351d5261c37e2e636ac2c9ec09a
Jane <friej715@newschool.edu>
2012-04-09 20:21:50 -0400

    Testing the cell

---
+++ Restaurants/ReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.h
+//  Restaurants
+//
+//  Created by Jane Friedhoff on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewViewController : UITableViewController
+
+@end
